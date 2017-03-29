33da5f193f3d26d64f85b1eb38c1fe7c08706c59
qql <530618790@qq.com>
2017-03-29 22:51:11 +0800

    taobao login

--- app/src/main/java/com/qql/dagger/recommend/fragment/MyFragment.java
+++ app/src/main/java/com/qql/dagger/recommend/fragment/MyFragment.java
@@ -27,6 +27,7 @@ import com.alibaba.baichuan.android.trade.callback.AlibcTradeInitCallback;
 import com.bigkoo.convenientbanner.ConvenientBanner;
 import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
 import com.bigkoo.convenientbanner.listener.OnItemClickListener;
+import com.bumptech.glide.Glide;
 import com.qql.dagger.recommend.R;
 import com.qql.dagger.recommend.animotion.ZoomInTransformer;
 import com.qql.dagger.recommend.base.BaseFragment;
@@ -34,6 +35,8 @@ import com.qql.dagger.recommend.model.bean.BannerBean;
 import com.qql.dagger.recommend.model.holder.NetImageHolder;
 import com.qql.dagger.recommend.presenter.HomePresenter;
 import com.qql.dagger.recommend.presenter.contract.HomeContract;
+import com.qql.dagger.recommend.tansform.GlideCircleTransform;
+import com.qql.dagger.recommend.tansform.GlideRoundTransform;
 import com.qql.dagger.recommend.utils.SnackbarUtil;
 import com.qql.dagger.recommend.utils.ToastUtil;
 
@@ -55,12 +58,13 @@ public class MyFragment extends Fragment {
     @BindView(R.id.login)
     ImageView imageView;
     private View view;
-    private String username;
+    private Session user = null;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         setHasOptionsMenu(true);
+        user = AlibcLogin.getInstance().getSession();
         super.onCreate(savedInstanceState);
     }
 
@@ -76,12 +80,13 @@ public class MyFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         mToolbar.inflateMenu(R.menu.menu_scrolling);
         mToolbar.bringToFront();
-        //使用CollapsingToolbarLayout必须把title设置到CollapsingToolbarLayout上，设置到Toolbar上则不会显示
-        mCollapsingToolbarLayout.setTitle(TextUtils.isEmpty(username)?"登录":username);
         //通过CollapsingToolbarLayout修改字体颜色
         mCollapsingToolbarLayout.setExpandedTitleColor(Color.WHITE);//设置还没收缩时状态下字体颜色
         mCollapsingToolbarLayout.setCollapsedTitleTextColor(Color.WHITE);//设置收缩后Toolbar上字体的颜色
         mCollapsingToolbarLayout.setCollapsedTitleGravity(Gravity.CENTER);
+        mCollapsingToolbarLayout.setCollapsedTitleTextAppearance(android.R.style.TextAppearance_DialogWindowTitle);
+        mCollapsingToolbarLayout.setExpandedTitleTextAppearance(android.R.style.TextAppearance_DialogWindowTitle);
+        refresh();
         super.onViewCreated(view, savedInstanceState);
     }
 
@@ -89,35 +94,45 @@ public class MyFragment extends Fragment {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.login:
-                //TODO to login
                 showLogin(view);
                 break;
         }
     }
     public void showLogin(View view) {
-        AlibcLogin.getInstance().showLogin(getActivity(), new AlibcLoginCallback() {
-            @Override
-            public void onSuccess() {
-                ToastUtil.show("success");
-            }
+        if (!AlibcLogin.getInstance().isLogin()) {
+            AlibcLogin.getInstance().showLogin(getActivity(), new AlibcLoginCallback() {
+                @Override
+                public void onSuccess() {
+                    ToastUtil.show("success");
+                }
 
-            @Override
-            public void onFailure(int i, String s) {
-                ToastUtil.show("failure");
+                @Override
+                public void onFailure(int i, String s) {
+                    ToastUtil.show("failure");
+                }
+            });
+        }
+        user  = AlibcLogin.getInstance().getSession();
+        refresh();
+    }
+
+    private void refresh(){
+        if (user != null) {
+            String url = user.avatarUrl;
+            String username = user.nick;
+            if (!TextUtils.isEmpty(url)) {
+                Glide.with(this).load(url)
+                        .bitmapTransform(new GlideCircleTransform(getActivity()))
+                        .into(imageView);
             }
-        });
-//        LoginService loginService = AlibabaSDK.getService(LoginService.class);
-//        loginService.showLogin(this, new LoginCallback() {
-//
-//            @Override
-//            public void onSuccess(Session session) {
-//                Toast.makeText(MyFragment.this, "-isLogin-"+session.isLogin()+"-UserId-"session.getUserId() + "-LoginTime-"+ session.getLoginTime()+"[user]:nick="+session.getUser().nick + "头像"+ session.getUser().avatarUrl,Toast.LENGTH_SHORT).show();
-//            }
-//            @Override
-//            public void onFailure(int code, String message) {
-//                Toast.makeText(MyFragment.this, "授权取消"+code+message,
-//                        Toast.LENGTH_SHORT).show();
-//            }
-//        });
+            if (!TextUtils.isEmpty(username)) {
+                mCollapsingToolbarLayout.setTitle(username);
+            }
+        } else {
+            mCollapsingToolbarLayout.setTitle("登录");
+            Glide.with(this).load(R.mipmap.login)
+                    .bitmapTransform(new GlideCircleTransform(getActivity()))
+                    .into(imageView);
+        }
     }
 }
